9c7d404cf690b176e3b9efc39d4fc26a6c3631a7
Adam Thomas <thomasadam83@hotmail.com>
2022-09-27 14:05:59 -0400

    Added page title example

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+import {BrowserRouter, Switch, Link} from 'react-router-dom';
+import {Page} from './extensions/Page';
+import React,{Component} from "react";
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <h1>React Navigation Sample</h1>
+        </header>
+        <nav>
+          <Link to="/">Home</Link>
+          <span> | </span>
+          <Link to="/about">About Me</Link>
+          <span> | </span>
+          <Link to="/contact">Contact</Link>
+        </nav>
+        <main>
+          <Switch>
+
+            <Page exact path="/" component={Home} title="Home"></Page>
+            <Page path="/about" component={About} title="About Me"></Page>
+            <Page path="/contact" component={Contact} title="Contact"></Page>
+          
+          </Switch>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
